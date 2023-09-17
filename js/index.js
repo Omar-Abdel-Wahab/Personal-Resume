76cc8f2c65d36a22a67d7dc85e5706070cfcc140
“Omar-Abdel-Wahab” <16p6034@eng.asu.edu.eg>
2023-09-17 17:18:12 +0300

    fix: menu doesn't hide when clicked outside
    Summary:
    
    The following commit contains menu that hides when user clicks outside
    its boundaries
    
    Reason:
    
    This commit is made to improve user experience
    
    Testing:
    
    I tested this manually
    
    Notes:
    
    a) Concerns:
    
    None
    
    b) Future commits can contain:
    
    Not determined yet
    
    reviewed-by: author

--- js/index.js
+++ js/index.js
@@ -4,8 +4,26 @@ const navbarContent = document.getElementById("navbarSupportedContent");
 const navbarItems = document.querySelectorAll("ul.navbar-nav > li");
 
 navbarItems.forEach((item) => {
-    item.addEventListener("click", function(){
-        navbarContent.classList.remove("show");
-        collapseButton.classList.add("collapsed");
-    })
+    item.addEventListener("click", hideMenu);
 });
+
+document.addEventListener("click", function(event){
+    const clickedElement = event.target;
+    if(clickedOutside(clickedElement)){
+        hideMenu();
+    }
+});
+
+function hideMenu(){
+    navbarContent.classList.remove("show");
+    collapseButton.classList.add("collapsed");
+}
+
+function clickedOutside(clickedElement){
+    const navbarToggler = clickedElement.classList.contains("navbar-toggler");
+    const customToggler = clickedElement.classList.contains("custom-toggler");
+    if(navbarToggler || customToggler){
+        return false;
+    }
+    return true;
+}
